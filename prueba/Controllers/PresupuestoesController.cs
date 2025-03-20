7c57d71ea00682666a643b93ca7a9e9dd477448c
Emmanuelpaz <manuelp1927@gmail.com>
2025-03-19 22:36:18 -0500

    pruebaepp

---
+++ prueba/Controllers/PresupuestoesController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using prueba.Models;
+
+namespace prueba.Controllers
+{
+    public class PresupuestoesController : Controller
+    {
+        private readonly PruebaContext _context;
+
+        public PresupuestoesController(PruebaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Presupuestoes
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Presupuestos.ToListAsync());
+        }
+
+        // GET: Presupuestoes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var presupuesto = await _context.Presupuestos
+                .FirstOrDefaultAsync(m => m.PresupuestoId == id);
+            if (presupuesto == null)
+            {
+                return NotFound();
+            }
+
+            return View(presupuesto);
+        }
+
+        // GET: Presupuestoes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Presupuestoes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("PresupuestoId,Unidad,TipoBienServicio,Cantidad,ValorUnitario,ValorTotal,FechaAdquision,Proveedor,Documentacion")] Presupuesto presupuesto)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(presupuesto);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(presupuesto);
+        }
+
+        // GET: Presupuestoes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var presupuesto = await _context.Presupuestos.FindAsync(id);
+            if (presupuesto == null)
+            {
+                return NotFound();
+            }
+            return View(presupuesto);
+        }
+
+        // POST: Presupuestoes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("PresupuestoId,Unidad,TipoBienServicio,Cantidad,ValorUnitario,ValorTotal,FechaAdquision,Proveedor,Documentacion")] Presupuesto presupuesto)
+        {
+            if (id != presupuesto.PresupuestoId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(presupuesto);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PresupuestoExists(presupuesto.PresupuestoId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(presupuesto);
+        }
+
+        // GET: Presupuestoes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var presupuesto = await _context.Presupuestos
+                .FirstOrDefaultAsync(m => m.PresupuestoId == id);
+            if (presupuesto == null)
+            {
+                return NotFound();
+            }
+
+            return View(presupuesto);
+        }
+
+        // POST: Presupuestoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var presupuesto = await _context.Presupuestos.FindAsync(id);
+            if (presupuesto != null)
+            {
+                _context.Presupuestos.Remove(presupuesto);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PresupuestoExists(int id)
+        {
+            return _context.Presupuestos.Any(e => e.PresupuestoId == id);
+        }
+    }
+}
---
+++ prueba/Models/PruebaContext.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace prueba.Models;
+
+public partial class PruebaContext : DbContext
+{
+    public PruebaContext()
+    {
+    }
+
+    public PruebaContext(DbContextOptions<PruebaContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Presupuesto> Presupuestos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+
+    }
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+ //       => optionsBuilder.UseSqlServer("server=localhost\\SQLEXPRESS; database=prueba; Encrypt=false; integrated security=true; TrustServerCertificate=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Presupuesto>(entity =>
+        {
+            entity.HasKey(e => e.PresupuestoId).HasName("PK__presupue__DAAAB25C24A246F5");
+
+            entity.ToTable("presupuesto");
+
+            entity.Property(e => e.PresupuestoId).HasColumnName("presupuesto_id");
+            entity.Property(e => e.Cantidad)
+                .HasMaxLength(100)
+                .HasColumnName("cantidad");
+            entity.Property(e => e.Documentacion)
+                .HasMaxLength(100)
+                .HasColumnName("documentacion");
+            entity.Property(e => e.FechaAdquision)
+                .HasMaxLength(100)
+                .HasColumnName("fechaAdquision");
+            entity.Property(e => e.Proveedor)
+                .HasMaxLength(100)
+                .HasColumnName("proveedor");
+            entity.Property(e => e.TipoBienServicio)
+                .HasMaxLength(100)
+                .HasColumnName("tipoBienServicio");
+            entity.Property(e => e.Unidad)
+                .HasMaxLength(100)
+                .HasColumnName("unidad");
+            entity.Property(e => e.ValorTotal)
+                .HasMaxLength(100)
+                .HasColumnName("valorTotal");
+            entity.Property(e => e.ValorUnitario)
+                .HasMaxLength(100)
+                .HasColumnName("valorUnitario");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
